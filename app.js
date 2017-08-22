92b6d087901a256ecfdd1930df0659e96edc7d7f
Bailey Bryant <bailey.bryant10@gmail.com>
2017-08-22 16:10:16 -0500

    use mongo to find todo list data and add(insert) a new todo

--- app.js
+++ app.js
@@ -2,35 +2,11 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const validator = require('express-validator');
-const data = [{
-    "text": "Walk dog",
-    "done": true,
-    id: 0
-  },
-  {
-    "text": "Wash Car",
-    "done": false,
-    id: 1
-  },
-  {
-    "text": "Buy oranges",
-    "done": false,
-    id: 2
-  },
-  {
-    "text": "Call Mom",
-    "done": false,
-    id: 3
-  },
-  {
-    "text": "Pay electrity bill",
-    "done": true,
-    id: 4
-  }
-]
-
-
+const MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 const app = express();
+let database;
+const url = 'mongodb://localhost:27017/todo';
 
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
@@ -44,44 +20,57 @@ app.use(bodyParser.urlencoded({
 }));
 //Gill doesn't know what the true and false part means. false = parses strings and arrays. true = parses nested objects. False is the default
 
+// db is a reference to the mongo db, we named it database in the boilerplate code, so it needs to be database; saved it off in the local  variable "database"
 
 app.get('/', function(req, res) {
-
-  res.render('list', {
-    pear: data
+  let collection = database.collection('todos');
+  // Find some documents
+    collection.find({}).toArray(function(err, todo) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(todo)
+    res.render('list', {pear: todo});
   });
 })
 // only need second part "pear: data" if you are passing data from this doc to the mustache file
 
 app.post("/todo", function(req, res) {
+  console.log('post to /todo')
   let add = req.body.newtodo;
-  let max = 0;
-  for (var i = 0; i < data.length; i++) {
-    if (max < data[i].id) {
-      max = data[i].id
-    }
-  }
+  // let max = 0;
+  // for (var i = 0; i < todo.length; i++) {
+  //   if (max < todo[i].id) {
+  //     max = todo[i].id
+  //   }
+  // }
 
-  let todo = {
+  let todo2 = {
     "text": add,
     "done": false,
-    id: max + 1
+    // id: max + 1
   };
-  data.push(todo);
+
+  let collection = database.collection('todos');
+  // Find some documents
+    collection.insertOne(todo2,function(err, todo) {
+    // assert.equal(err, null);
+    console.log("added new to");
+    console.log("theobject", todo)
 
 
   res.redirect('/');
+});
 })
 
 app.post("/mac/:id", function(req, res) {
   let moveid = parseInt(req.params.id);
 
-  for (var i = 0; i < data.length; i++) {
+  for (var i = 0; i < todo.length; i++) {
 
-    if (moveid === data[i].id) {
-      data[i].done = true;
+    if (moveid === todo[i].id) {
+      todo[i].done = true;
     }
-    console.log(data[i].id);
+    console.log(todo[i].id);
   }
   // console.log(move);
 
@@ -90,10 +79,27 @@ app.post("/mac/:id", function(req, res) {
 
 
   res.render('list', {
-    pear: data
+    pear: todo
   });
 })
 
 app.listen(3000, function() {
   console.log('Successfully started express application!');
 })
+
+
+
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to mongodb");
+  database = db;
+});
+
+process.on('SIGINT', function() {
+  console.log("\nshutting down");
+  database.close(function () {
+    console.log('mongodb disconnected on app termination');
+    process.exit(0);
+  });
+});
